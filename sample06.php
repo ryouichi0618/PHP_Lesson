2b1a49e7ed2d4c6f266961099d82e3f556333396
ryouichi0618 <tennis.ryouichi@gmail.com>
2021-06-17 00:00:07 +0900

    php1

--- sample06.php
+++ sample06.php
@@ -12,14 +12,34 @@
 </head>
 <body>
 <header>
-<h1 class="font-weight-normal">PHP</h1>    
+<h1 class="font-weight-normal">PHP</h1>
 </header>
 
 <main>
 <h2>Practice</h2>
 <pre>
+<?php
+$i =1;
+print($i . "\n");
+$i = $i +1;
+print($i . "\n");
+?>
 
+--------
+
+<?php
+// $i =  1;
+
+// while ($i <= 365) {
+//   print($i . "\n");
+//   $i++;
+// }
+for ($i=1; $i<=365; $i++) {
+  print($i . "\n");
+}
+
+?>
 </pre>
 </main>
-</body>    
+</body>
 </html>
